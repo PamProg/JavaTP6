8b354a7f126ebbd57368e616180908c422f6ae3c
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-07-06 14:21:13 +0200

    second commit

--- src/fr/pizzeria/dao/IPizzaDao.java
+++ src/fr/pizzeria/dao/IPizzaDao.java
@@ -4,9 +4,26 @@ import fr.pizzeria.model.Pizza;
 
 public interface IPizzaDao {
 
+	/**
+	 * 
+	 * @return la liste des pizzas stockées
+	 */
 	Pizza[] findAllPizzas();
-	boolean saveNexPizza(Pizza pizza);
-	boolean updatePizza(String codePizza, Pizza pizza);
-	boolean deletePizza(String codePizza);
 	
+	/**
+	 * 
+	 * @param pizza la pizza à stocker
+	 * @return true si le stockage s'est bien passé, false sinon
+	 */
+	boolean saveNexPizza(Pizza pizza);
+	
+	/**
+	 * 
+	 * @param codePizza le code de la pizza à mettre à jour
+	 * @param pizza
+	 * @return
+	 */
+	boolean updatePizza(String codePizza, Pizza pizza);
+	
+	boolean deletePizza(String codePizza);
 }
